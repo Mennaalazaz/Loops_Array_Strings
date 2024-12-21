f3613330381dd4b511263ec2ff2f23675e14fbde
Menna <162923696+Mennaalazaz@users.noreply.github.com>
2024-12-21 03:23:39 +0200

    Update main.cpp

--- Harder Problem/main.cpp
+++ Harder Problem/main.cpp
@@ -8,27 +8,32 @@ void fastIO() {
     cout.tie(NULL);
 }
 
-int freq[200006];
-void solve() {
-    int n; cin >> n;
-    int a[n], b[n];
-    for (int i = 0; i < n; i++) cin >> a[i];
 
-    int avilable = 0;
-    for (int i = 0; i < n; i++) {
-        if (freq[a[i]] == 0) {
-            b[i] = a[i];
-            freq[a[i]]++;
-        }
-        else {
-            while(freq[avilable]) {
-                avilable++; //loop till find number its frequency=0
+void solve() {
+    int t; cin >> t;
+    while (t--) {
+        int n; cin >> n;
+        int a[n], b[n];int freq[200006]={};
+        for (int i = 0; i < n; i++) cin >> a[i];
+
+        int avilable = 1;
+        for (int i = 0; i < n; i++) {
+            if (freq[a[i]] == 0) {
+                b[i] = a[i];
+                freq[a[i]]++;
             }
-            b[i] = avilable;
-            freq[avilable]++;
+            else {
+                while(freq[avilable]) {
+                    avilable++; //loop till find number its frequency=0
+                }
+                b[i] = avilable;
+                freq[avilable]++;
+            }
         }
+        for (int i = 0; i < n; i++) cout << b[i] << ' ';
+        cout << endl;
     }
-    for (int i = 0; i < n; i++) cout << b[i] << " ";
+
 }
 
 
